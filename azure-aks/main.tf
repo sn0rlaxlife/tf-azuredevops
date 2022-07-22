b88c476831d4ebd690b04cfc8c5bae4dddf01132
sn0rlax <79283899+sn0rlaxlife@users.noreply.github.com>
2022-07-22 10:16:31 -0700

    Update main.tf

--- azure-aks/main.tf
+++ azure-aks/main.tf
@@ -2,8 +2,24 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "~>3.14.0"
+      version = "=2.46.0"
     }
+  } 
+  backend "azurerm" {
+    resource_group_name  = "preview-devops"
+    storage_account_name = "azucar1"
+    container_name       = "terraform"
+    key                  = "terraform.tfstate"
   }
+ 
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "preview-devops" {
+  name     = "preview-devops"
+  location = "centralus"
 }
 
