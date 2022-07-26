cc8716eea3cfb4d1f361d60574323692cefe95f0
sn0rlax <79283899+sn0rlaxlife@users.noreply.github.com>
2022-07-25 20:34:14 -0700

    Update main.tf
    updated with tf version

--- azure-aks/main.tf
+++ azure-aks/main.tf
@@ -1,23 +1,39 @@
 terraform {
+  #version of tf
+  required_version = ">= 0.13"
+  #providers in use
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "=2.46.0"
+      source  = "hashicorp/azurerm"
+      version = ">=2.46.0"
     }
-  } 
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+    
   backend "azurerm" {
-    resource_group_name  = "preview-devops"
-    storage_account_name = "azucar1"
-    container_name       = "terraform"
-    key                  = "terraform.tfstate"
+    #resource_group_name  = "preview-devops"
+    #storage_account_name = "azucar1"
+    #container_name       = "terraform"
+    #key                  = "terraform.tfstate"
   }
- 
-}
-
+ }
+# - AzureRM Provider Block that communicates with Azure
 provider "azurerm" {
-  features {}
+  features {
+  
+  }
 }
 
+resource "random_pet" "aksrandom" {
+  
+}
+  
 resource "azurerm_resource_group" "preview-devops" {
   name     = "preview-devops"
   location = "centralus"
